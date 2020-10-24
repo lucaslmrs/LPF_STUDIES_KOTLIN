9a8afeb7e47509b241e08ba0d1a45a9ccca20a60
Lucas dos Santos <rlucasmatheus@gmail.com>
2020-10-24 00:23:46 -0300

    questao03.kt

---
+++ src/exercicios_anotacoes/lista_de_exercicios_1/questao03/questao03.kt
@@ -0,0 +1,32 @@
+package exercicios_anotacoes.lista_de_exercicios_1.questao03
+
+/*
+TODO: 3) Implemente a função de interseção entre duas listas, ou seja, que retorna apenas os
+         elementos que estão presentes em ambas.
+ */
+
+
+class List<C>(val info: C?, val next: List<C>?){
+    @JvmOverloads
+    fun toString(lista: List<C>? = this): String{ return if (lista != null) "${lista.info}, ${toString(lista.next)}" else "" }
+    fun imprimir() { print(this.toString(this)) }
+    fun contains(e: C, lista: List<C>? = this): Boolean{
+        return if(lista != null)
+            if (e!!.equals(lista.info)) true else contains(e, lista.next)
+        else false
+    }
+}
+
+fun intersection(a: List<Int>?, b: List<Int>?): List<Int>? {
+    return if (a == null || b == null) null
+    else if (b.contains(a.info!!)) List(a.info, intersection(a.next, b))
+    else intersection(a.next, b)
+}
+
+
+fun main(){
+    val a = List(15, List(30, List(12, List(24, List(48, List(100, null))))))
+    val b = List(100, List(12, List(24, List(50, null))))
+    intersection(a, b)!!.imprimir()
+}
+
