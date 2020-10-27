cdb7cbc3c400831f0b1a0b9679f1cb3676153d19
Lucas dos Santos <rlucasmatheus@gmail.com>
2020-10-27 19:00:07 -0300

    lista_de_exercicios_2/questao09

---
+++ src/exercicios_anotacoes/lista_de_exercicios_2/questao09/questao09.kt
@@ -0,0 +1,59 @@
+package exercicios_anotacoes.lista_de_exercicios_2.questao09
+
+/*
+TODO:
+ 9) Defina uma função que, dada uma lista e um valor, retorne um par de listas em que a
+ primeira contém todos os elementos da lista inferiores a esse valor e a segunda lista
+ contém todos os outros elementos.
+ */
+
+
+class List(val info: Int, val next: List?){
+    @JvmOverloads
+    fun toString(lista: List? = this): String {
+        fun toStringn(lista: List? = this): String{
+            return if (lista != null) "${lista.info}, ${toStringn(lista.next)}"
+            else ""
+        }
+        return "[${toStringn(this).substringBeforeLast(',')}]"
+    }
+    fun imprimir() { println(this.toString()) }
+}
+
+
+fun listOf(vararg elems: Int):List? {
+    return  if (elems.isNotEmpty()) List(elems[0], listOf(*elems.copyOfRange(1, elems.size))) else null
+}
+
+fun filter(f: (Int)->Boolean, lista: List?): List?{
+    return if (lista != null){
+        if (f(lista.info))
+            List(lista.info, filter(f, lista.next))
+        else filter(f, lista.next)
+    } else null
+}
+
+fun twoFilters(f: (Int)->Boolean, lista: List?): Pair<List?, List?>{
+    val g = {x: Int -> !f(x)}
+    return Pair(filter(f, lista), filter(g, lista))
+}
+
+
+fun main(){
+    val f = {x:Int -> x<4}
+
+    println(twoFilters(f, listOf(1,2,3,4)))
+    println(twoFilters(f, listOf()))
+    println(twoFilters(f, listOf(2,2,2,2)))
+    println(twoFilters(f, listOf(5,5,5,5)))
+    println(twoFilters(f, listOf(-2,-2,-2,-2)))
+    println(twoFilters(f, listOf(7,5,54,34,35,3,2,4665,8485,4858,-255423)))
+}
+
+
+
+
+
+
+
+
